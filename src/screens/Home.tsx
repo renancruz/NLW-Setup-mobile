43cc5eb825e589dd94e85ec92d45f28768dfd6e2
Renan Cruz <renan.frcr@gmail.com>
2023-01-21 14:41:45 -0300

    finalizando o layout mobile

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { Text, View, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { genetateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning'
 
@@ -11,6 +12,8 @@ const minimumSummaryDateSizes = 18 * 7;
 const amountOfDaysToFill = minimumSummaryDateSizes - datesFromYearStart.length
 
 export function Home() {
+  const { navigate } = useNavigation()
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header/>
@@ -36,6 +39,7 @@ export function Home() {
             datesFromYearStart.map(date => (
               <HabitDay
                 key={date.toISOString()}
+                onPress={() => navigate('habit', {date: date.toISOString()})}
               />
             ))
           }
